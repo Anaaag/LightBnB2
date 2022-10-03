6d1cd5b1e9036796368c522ff92925b05182d0ab
Anaaag <agarc20@outlook.com>
2022-10-03 14:28:03 -0700

    added data

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO users (id, name, email, password)
+VALUES (1, 'Marisa', 'marisa@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+(2, 'Camille', 'camille@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+(3, 'Serena', 'serena@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+
+
+
+
